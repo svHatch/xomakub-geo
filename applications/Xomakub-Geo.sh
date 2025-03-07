fc1cfcda7711e4b647d73be8b3dc872a8f60cf1d
svHatch <scott.v.hatcher@pm.me>
2025-03-07 11:36:25 -0400

    Modified application launchers and removed unused ones.

---
+++ applications/Xomakub-Geo.sh
@@ -0,0 +1,12 @@
+cat <<EOF >~/.local/share/applications/Xomakub-Geo.desktop
+[Desktop Entry]
+Version=1.0
+Name=Xomakub-Geo
+Comment=Xomakub-Geo Controls
+Exec=xfce4-terminal --title=Xomakub-Geo -e xomakub-geo
+Terminal=false
+Type=Application
+Icon=/home/$USER/.local/share/xomakub-geo/applications/icons/Xomakub-Geo.png
+Categories=GTK;
+StartupNotify=false
+EOF
