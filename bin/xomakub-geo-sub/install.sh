7a2e770a7720e782677a35aa41370050f184d998
svHatch <scott.v.hatcher@pm.me>
2025-03-11 07:49:57 -0300

    Removed a bunch of install optional apps

--- bin/xomakub-geo-sub/install.sh
+++ bin/xomakub-geo-sub/install.sh
@@ -3,18 +3,7 @@ CHOICES=(
   "Dev Database      Install development database in Docker"
   "1password         Manage your passwords securely across devices"
   "Audacity          Record and edit audio"
-  "ASDControl        Set brightness on Apple Studio and XDR displays via cli"
-  "Brave             Chrome-based browser with built-in ad blocking"
-  "Cursor            The AI Code Editor"
-  "Doom Emacs        Emacs framework with curated list of packages"
-  "Dropbox           Sync files across computers with ease"
-  "Mainline Kernels  Install newer Linux kernels than Ubuntu defaults"
   "OBS Studio        Record screencasts with inputs from both display + webcam"
-  "Ollama            Run LLMs, like Meta's Llama3, locally"
-  "Steam             Play games from Valve's store"
-  "VirtualBox        Virtual machines to run Windows/Linux"
-  "Zed               Fast all-purpose editor"
-  "Zoom              Attend and host video chat meetings"
   "> All             Re-run any of the default installers"
   "<< Back           "
 )
@@ -37,7 +26,6 @@ else
   case "$INSTALLER" in
   "dev-language") INSTALLER_FILE="$XOMAKUB_PATH/install/terminal/select-dev-language.sh" ;;
   "dev-database") INSTALLER_FILE="$XOMAKUB_PATH/install/terminal/select-dev-storage.sh" ;;
-  "ollama") INSTALLER_FILE="$XOMAKUB_PATH/install/terminal/optional/app-ollama.sh" ;;
   *) INSTALLER_FILE="$XOMAKUB_PATH/install/desktop/optional/app-$INSTALLER.sh" ;;
   esac
 
